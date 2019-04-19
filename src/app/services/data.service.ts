4ef8c083ca4c21d54bb97ed70f95668560d3611e
brangr <abrunenko@gmail.com>
2019-04-19 16:56:04 +0300

    material spinner

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,7 +8,7 @@ import { HexService } from './hex.service'
 })
 export class DataService {
     private proxyUrl = 'https://pocketnet.app:8888/rpc';
-    // private proxyUrl = 'https://216.108.237.11:8888/rpc';
+    //private proxyUrl = 'https://192.168.0.11:8888/rpc';
     private node = '2';
 
     constructor(private http: HttpClient, private hex: HexService) { }
