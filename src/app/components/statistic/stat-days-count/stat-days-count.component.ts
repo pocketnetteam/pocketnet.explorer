b2b73d71e02393fc707d8500f7f5f32fee158ee0
brangr <andy.oknen@gmail.com>
2019-08-05 17:14:40 +0300

    Users count statistic

--- src/app/components/statistic/stat-days-count/stat-days-count.component.ts
+++ src/app/components/statistic/stat-days-count/stat-days-count.component.ts
@@ -36,11 +36,12 @@ export class StatDaysCountComponent implements OnInit, AfterViewInit {
 
         this.dataService.getStatistic(end_time, beg_time).subscribe(data => {
             this.statisticData = data['data']['result'];
-            this.fillChart(this.statisticData);
+            this.fillChartUsers(this.statisticData);
+            this.fillChartEvents(this.statisticData);
         });
     }
 
-    fillChart(data) {
+    fillChartUsers(data) {
         let _datasets = {};
         let categories = [];
 
@@ -51,6 +52,111 @@ export class StatDaysCountComponent implements OnInit, AfterViewInit {
             for (let y in _x) {
                 if (!(y in _datasets)) {
                     let _caption = y;
+                    if (_caption != 'UsersAcc') continue;
+                    if (_caption == 'UsersAcc') _caption = 'Users count';
+
+                    _datasets[y] = {
+                        name: _caption,
+                        data: []
+                    };
+                }
+
+                _datasets[y].data.push(_x[y]);
+            }
+        }
+
+        let datasets = [];
+        for (let d in _datasets) {
+            datasets.push(_datasets[d]);
+        }
+
+        Highcharts.chart('stat_days_users_canvas', {
+            title: {
+                text: ''
+            },
+            chart: {
+                type: 'spline'
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                }
+            },
+            xAxis: {
+                categories: categories,
+                type: 'datetime',
+                labels: {
+                    format: '{value:%e %b}',
+                    style: {
+                        fontSize: '0.6rem'
+                    }
+                },
+            },
+            legend: {
+                enabled: true,
+                layout: 'horizontal',
+                align: 'center',
+                verticalAlign: 'bottom'
+            },
+            tooltip: {
+                shared: true,
+                crosshairs: true,
+                // formatter: function() {
+                //     return  `<b>${ this.series.name }</b> ${ Highcharts.dateFormat('%b %e, %Y', this.x) }<br/><b>${ this.y }</b> tx(s)`;
+                // },
+                formatter: function () {
+                    var points = this.points;
+                    var pointsLength = points.length;
+                    var tooltipMarkup = pointsLength ? `<span style="font-size: 12px"><b>${ Highcharts.dateFormat('%b %e, %Y', points[0].key) }</b></span><br/>` : ``;
+                    var index;
+
+                    for (index = 0; index < pointsLength; index += 1) {
+                        tooltipMarkup += `<span style="color:${ points[index].color }">\u25CF</span> ${ points[index].series.name }: <b>${ points[index].y }</b><br/>`;
+                    }
+
+                    return tooltipMarkup;
+                }
+            },
+            plotOptions: {
+                spline: {
+                    marker: {
+                        enabled: false
+                    }
+                },
+                series: {
+                    connectNulls: true
+                }
+            },
+            series: datasets,
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+        });
+    }
+
+    fillChartEvents(data) {
+        let _datasets = {};
+        let categories = [];
+
+        for (let x in data) {
+            let _x = data[x];
+            categories.push(new Date(+x * 1000));
+
+            for (let y in _x) {
+                if (!(y in _datasets)) {
+                    let _caption = y;
+                    if (_caption == 'UsersAcc') continue;
                     if (_caption == 'Users') _caption = 'New users';
                     if (_caption == 'Subscribes') _caption = 'Follows';
 
