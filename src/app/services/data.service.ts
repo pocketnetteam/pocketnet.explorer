62fc8cf6cada905e43b1ba525fa3dadb78887504
Andy Oknen <andy.oknen@yandex.ru>
2021-08-23 10:50:43 +0300

    1

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -37,7 +37,7 @@ export class DataService {
         }
     ];
 
-    public currentLocation: any = window.location.host.split(":")[0] + ":38081";
+    public currentLocation: any = 'localhost:38081'; // window.location.host.split(":")[0] + ":38081";
     public useProxy: string | null = localStorage.getItem('explorerUseProxy')
 
     public proxy = proxy ? JSON.parse(proxy) : this.defaultProxies[0]
