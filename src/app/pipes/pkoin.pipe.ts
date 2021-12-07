9c78cd378d08ecc95efcdfce6a47479e6eacb7ea
Andy Oknen <andy.oknen@yandex.ru>
2021-12-07 10:08:29 +0300

    2

--- src/app/pipes/pkoin.pipe.ts
+++ src/app/pipes/pkoin.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PKoinPipe implements PipeTransform {
 
-    transform(value: number, minFraction: number = 0, maxFraction: number = 8): string {
+    transform(value: number, minFraction: number = 8, maxFraction: number = 8): string {
         let options = { minimumFractionDigits: minFraction, maximumFractionDigits: maxFraction };
         return `${value.toLocaleString('en-us', options)} PKOIN`;
     }
