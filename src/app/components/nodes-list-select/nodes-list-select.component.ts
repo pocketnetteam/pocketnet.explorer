d9e6b13b6e5663f875739ea02f800c6f9fe2e4a9
Andy Oknen <andy.oknen@yandex.ru>
2021-12-28 12:49:30 +0300

    1

--- src/app/components/nodes-list-select/nodes-list-select.component.ts
+++ src/app/components/nodes-list-select/nodes-list-select.component.ts
@@ -59,7 +59,7 @@ export class NodesListSelectComponent implements OnInit {
                 for (var n in res.info.nodeManager.nodes)
                 {
                     let node = res.info.nodeManager.nodes[n];
-                    if (node.node.version && node.node.version.startsWith("0.20.16"))
+                    if (node.node.version && node.node.version.startsWith("0.20"))
                         if (node.status.difference > -10)
                             nodes[node.node.key] = node;
                 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -269,7 +269,7 @@ export class DataService {
                 for (var n in res.info.nodeManager.nodes)
                 {
                     let node = res.info.nodeManager.nodes[n];
-                    if (node.node.version && !node.node.version.startsWith("0.20.16"))
+                    if (node.node.version && !node.node.version.startsWith("0.20"))
                         if (node.status.difference > -10)
                             nodes[node.node.key] = node;
                 }
