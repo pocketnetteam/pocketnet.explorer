cad2992a3ed855b991a5b48056226185112a9861
Andy Oknen <andy.oknen@yandex.ru>
2022-12-21 19:47:32 +0300

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -64,13 +64,6 @@ export class DataService {
             wss: '8099',
             key: 'test.pocketnet.app:8899:8099',
             default: true
-        }, 
-        {
-            host: 'test.pocketnet.app', 
-            port: '8899',
-            wss: '8099',
-            key: 'test.pocketnet.app:8899:8099',
-            default: true
         }
     ];
 
